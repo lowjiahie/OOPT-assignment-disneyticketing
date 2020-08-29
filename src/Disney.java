ffcd3a7ec9e6b81e29f5033b7487c1d217434492
khorwenxin <66874971+khorwenxin@users.noreply.github.com>
2020-08-29 15:07:58 +0800

    Update Disney.java

--- src/Disney.java
+++ src/Disney.java
@@ -49,7 +49,7 @@ public class Disney {
         //Customer
         ArrayList<Customer> cust = new ArrayList<Customer>();
         cust.add(new Customer(person.get(3), "Premium", "Halloween", 83928102, 2, "07-09-2020 13:30:00" , 1, "Card", "123456789", 88.88));
-        cust.add(new Customer(person.get(4), "Classic", "Christmas", 0, 4, "07-09-2020 15:40:00", 1, "Cash", "1294877789", 176.66));
+        cust.add(new Customer(person.get(4), "Classic", "Christmas", 45263662, 4, "07-09-2020 15:40:00", 1, "Cash", "NULL", 176.66));
         
         //Package
         ArrayList<Package> pk = new ArrayList<Package>();
